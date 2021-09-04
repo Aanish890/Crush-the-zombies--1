1df219d27f32ff461376d8240ece700a622729ad
Aanish890 <85690660+Aanish890@users.noreply.github.com>
2021-09-04 12:06:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var jointLink
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+
+  Matter.Composite.add(bridge.body, jointPoint);
+  jointLink = new Link(bridge, jointPoint);
+
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+}
